dd8e47dc1ab5073ff24dd033b9811f66e1ea346f
fsalle1954 <fsalle2002@gmail.com>
2024-08-21 07:37:00 +0200

    test000054

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,62 @@
-import React from "react";
-
+// src » components   Menujsx   Menu
+import { UserContext } from "../context/UserContext";
+import { useContext } from "react";
+import axios from "axios";
+import { Link, useNavigation } from "react-router-dom";
 function Menu() {
-  return <div>Menu</div>;
+  const { user } = useContext(UserContext);
+  const { setUser } = useContext(UserContext);
+  const navigate = useNavigation();
+  const handleLogout = async () => {
+    try {
+      const res = await axios.get("/api/auth/logout", {
+        withCredentials: true,
+      });
+      setUser(null);
+      navigate("login");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="bg-black w-[200px] z-10 flex flex-col items-start absolute top-12 right-6 md:right-32 rounded-md p-4 space-y-4">
+      {!user && (
+        <h3 className=" text-white text-sm hover:text-gray-500  cursor-pointer ">
+          <Link to="/login">Login</Link>
+        </h3>
+      )}
+      {!user && (
+        <h3 className=" text-white text-sm hover:text-gray-500  cursor-pointer ">
+          <Link to="/register">Register</Link>
+        </h3>
+      )}
+      {user && (
+        <h3 className=" text-white text-sm hover:text-gray-500  cursor-pointer ">
+          <Link to={"/profile/" + user._id}>Profile</Link>
+        </h3>
+      )}
+      {user && (
+        <h3 className=" text-white text-sm hover:text-gray-500  cursor-pointer ">
+          <Link to={"/write"}>write</Link>
+        </h3>
+      )}
+
+      {user && (
+        <h3 className=" text-white text-sm hover:text-gray-500  cursor-pointer ">
+          <Link to={"/myblogs/" + user._id}>My Blogs</Link>
+        </h3>
+      )}
+      {user && (
+        <h3
+          className=" text-white text-sm hover:text-gray-500  cursor-pointer "
+          onClick={handleLogout}
+        >
+          Logout
+        </h3>
+      )}
+    </div>
+  );
 }
 
 export default Menu;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,83 @@
-import React from "react";
+// Navbar
+
+import React, { useContext, useState } from "react";
+import { Link, useLocation, useNavigation } from "react-router-dom";
+import BsSearch from "react-icons/bs";
+import FaBars from "react-icons/fa";
+import Menu from "./Menu";
+import UserContext from "../context/UserContext";
 
 function Navbar() {
-  return <div>Navbar</div>;
-}
+  const [prompt, setPrompt] = useState("");
+  const [menu, setMenu] = useState(false);
+  const navigate = useNavigation();
 
+  const path = useLocation().pathname;
+
+  const showMenu = () => {
+    setMenu(!menu);
+  };
+  const { user } = useContext(UserContext);
+
+  return (
+    <div>
+      <div className=" flex items-center justify-between px-6 md:px-[200px] py-4 bg-black text-white ">
+        <h1 className="text-lg md:text-xl font-extrabold">
+          {" "}
+          <Link to="/">Blogosphere </Link>
+        </h1>
+        {path === "/" && (
+          <div
+            onChange={(e) => setPrompt(e.target.value)}
+            className="flex justify-center items-center space-x-0"
+          >
+            <input
+              className="outlin-none rounded-l-xl px-3 text-black bg-white"
+              placeholder="Search a pst "
+              type="text"
+            />
+            <p
+              onClick={() =>
+                navigate(prompt ? "?search" + prompt : navigate("/"))
+              }
+              className="cursor-pointer p-1 bg-white text-black rounded-r-xl"
+            >
+              <BsSearch />
+            </p>
+          </div>
+        )}
+        <div className="hidden md:flex items-center justify-center space-x-2 md:space-x-4 ">
+          {user ? (
+            <h3>
+              {" "}
+              <Link to="/write">Write</Link>{" "}
+            </h3>
+          ) : (
+            <h3>
+              <Link to="/login">Login</Link>
+            </h3>
+          )}
+
+          {user ? (
+            <div onClick={showMenu}>
+              <p className="cursor-pointer relative"> </p>
+              <FaBars />
+              {Menu && <Menu />}
+            </div>
+          ) : (
+            <h3>
+              <Link to="/register">Register</Link>{" "}
+            </h3>
+          )}
+        </div>
+        <div onClick={showMenu} className="md:hidden text-lg">
+          <p className="cursor-pointer relative">
+            <FaBars />
+          </p>
+          {Menu && <Menu />}
+        </div>
+      </div>
+    </div>
+  );
+}
 export default Navbar;
